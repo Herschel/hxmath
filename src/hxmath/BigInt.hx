07458c49a872aac53dc647f90a57d4aec8728e5e
Mike Welsh <mwelsh@gmail.com>
2013-04-19 04:12:48 -0400

    BIGINT: Replace implementation class with typedef
    Instead of placing all of the implementation methods
    inside the wrapped type _BigInt, place the methods
    in the abstract instead. This way, we don't have to
    duplicate the interface to forward all method to
    _BigInt. _BigInt just becomes a POD object.

--- src/hxmath/BigInt.hx
+++ src/hxmath/BigInt.hx
@@ -2,86 +2,57 @@ package hxmath;
 
 // BigInt interface
 abstract BigInt(_BigInt) {
-	inline function new(n : _BigInt) { this = n; }
-
-	@:from
-	public static inline function ofInt(n : Int) : BigInt
-		return new BigInt( _BigInt.ofInt(n) );
-
-	public static inline function ofFloat(n : Float) : BigInt
-		return new BigInt( _BigInt.ofFloat(n) );
-
-	public static inline function ofString(n : String) : BigInt
-		return new BigInt( _BigInt.ofString(n) );
-
-	public inline function toInt() : Int
-		return this.toInt();
-
-	public inline function toFloat() : Float
-		return this.toFloat();
-
-	public inline function toString() : String
-		return this.toString();
-}
-
-/** Implementation of BigInt */
-private class _BigInt {
 	static inline var BITS_PER_CHUNK : Int = 31;
 	static inline var CHUNK_MASK : Int = (1 << BITS_PER_CHUNK) - 1;
 	static inline var CHUNK_MAX_FLOAT : Float = (1 << (BITS_PER_CHUNK-1)) * 2.0;
 
-	var _chunks : Array<Int>;
-	var _signum : Int;
-
-	function new() {
-		_chunks = new Array();
-		_signum = 0;
-	}
+	inline function new(n : _BigInt) { this = n; }
 
 	// INSTANTIATION
-	public static inline function alloc() : _BigInt {
-		return new _BigInt();
+	private static inline function alloc() : BigInt {
+		return new BigInt( {chunks: new Array(), signum: 0} );
 	}
 
-	public static function ofInt(n : Int) : _BigInt {
+	@:from
+	public static function ofInt(n : Int) : BigInt {
 		var bn = alloc();
 		if (n < 0) {
-			bn._signum = -1;
+			bn.signum = -1;
 			n = -n;
 		} else if (n > 0) {
-			bn._signum = 1;
+			bn.signum = 1;
 		} else {
 			return bn;
 		}
 		
 		while(n != 0) {
-			bn._chunks.push(n & CHUNK_MASK);
+			bn.chunks.push(n & CHUNK_MASK);
 			n >>>= BITS_PER_CHUNK;
 		}
 
 		return bn;
 	}
 
-	public static function ofFloat(n : Float) : _BigInt {
+	public static function ofFloat(n : Float) : BigInt {
 		var bn = alloc();
 
 		if (n < 0) {
-			bn._signum = -1;
+			bn.signum = -1;
 			n = -n;
 		} else if (n > 0) {
-			bn._signum = 1;
+			bn.signum = 1;
 		}
 
 		n = Math.ffloor(n);
 		while(n != 0) {
-			bn._chunks.push( Std.int(n % CHUNK_MAX_FLOAT) );
+			bn.chunks.push( Std.int(n % CHUNK_MAX_FLOAT) );
 			n = Math.ffloor( n / CHUNK_MAX_FLOAT );
 		}
 
 		return bn;
 	}
 
-	public static function ofString(n : String) : _BigInt {
+	public static function ofString(n : String) : BigInt {
 		// TODO
 		return alloc();
 	}
@@ -89,28 +60,33 @@ private class _BigInt {
 	// CONVERSION
 	public function toInt() : Int {
 		var n = 0;
-		var i = _chunks.length - 1;
+		var i = this.chunks.length - 1;
 		while(i >= 0) {
 			n <<= BITS_PER_CHUNK;
-			n |= _chunks[i];
+			n |= this.chunks[i];
 			i--;
 		}
-		return n * _signum;
+		return n * this.signum;
 	}
 
 	public function toFloat() : Float {
 		var n = 0.0;
-		var i = _chunks.length - 1;
+		var i = this.chunks.length - 1;
 		while(i >= 0) {
 			n *= CHUNK_MAX_FLOAT;
-			n += _chunks[i];
+			n += this.chunks[i];
 			i--;
 		}
-		return n * _signum;
+		return n * this.signum;
 	}
 
 	public function toString() : String {
 		// TODO
 		return "0";
 	}
+}
+
+private typedef _BigInt = {
+	var chunks : Array<Int>;
+	var signum : Int;
 }
