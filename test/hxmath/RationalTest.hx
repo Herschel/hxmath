4513aa707281c4be8e99b559eebb954bd8b2975f
Mike Welsh <mwelsh@gmail.com>
2013-04-20 03:37:19 -0400

    RATIONAL: Add tests for toFloat, toString, 0 numerator

--- test/hxmath/RationalTest.hx
+++ test/hxmath/RationalTest.hx
@@ -9,6 +9,10 @@ class RationalTest {
 		Assert.areEqual(0, r.num);
 		Assert.areEqual(1, r.den);
 
+		r = new Rational(0, -999);
+		Assert.areEqual(0, r.num);
+		Assert.areEqual(1, r.den);
+
 		r = new Rational(1, 5);
 		Assert.areEqual(1, r.num);
 		Assert.areEqual(5, r.den);
@@ -32,5 +36,41 @@ class RationalTest {
 		r = new Rational(4325, -801855);
 		Assert.areEqual(r.num, -5);
 		Assert.areEqual(r.den, 927);		
+	}
+
+	@Test
+	public function testFloatCasts() {
+		var r : Rational = new Rational(1, 2);
+		Assert.areEqual(1/2, r.toFloat());
+
+		r = new Rational(-5, 20);
+		Assert.areEqual(-1/4, r.toFloat());
+
+		r = new Rational(-2, -3);
+		Assert.areEqual(2/3, r.toFloat());
+
+		r = new Rational(53253, -213503);
+		Assert.areEqual(-53253/213503, r.toFloat());
+
+		r = new Rational(0, -999);
+		Assert.areEqual(0.0, r.toFloat());
+	}
+
+	@Test
+	public function testStringCasts() {
+		var r:Rational = new Rational(1, 2);
+		Assert.areEqual("1 / 2", r.toString());
+
+		r = new Rational(-4, 30);
+		Assert.areEqual("-2 / 15", r.toString());
+
+		r = new Rational(3, -33);
+		Assert.areEqual("-1 / 11", r.toString());
+
+		r = new Rational(0, -11);
+		Assert.areEqual("0 / 1", r.toString());
+
+		r = new Rational(-55, -1234);
+		Assert.areEqual("55 / 1234", r.toString());
 	}
 }
